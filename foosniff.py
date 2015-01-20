071d4c1ae8b09bacc3e4f46d2acb12f89f408700
maxigas <maxigas@anargeek.net>
2015-01-20 15:20:52 +0100

    next version

--- foosniff.py
+++ foosniff.py
@@ -2,22 +2,34 @@
 
 # 1. Import libraries and functions
 
+from sys import stdout
 from pattern.web import Twitter, plaintext
+from pattern.db import Datasheet
 
 # 2. Set variables
 
 keyword = "cybernetics"
+count   = 1000
 # keyword = "algorithm"
 
 # 3. Initialise
 
 twitter = Twitter(language='en')
+#ds = Datasheet()
 
 # 4. Get data
 
-for tweet in twitter.search(keyword, cached=False):
-	t = plaintext(tweet.text).replace('\n','') + '|||\n'
-	print plaintext(t)
-	with open('corpus.txt', 'a') as f:
-		f.write(t.encode('utf-8'))
-		
+# Twitter.search() returns up to 3000 results for a given query (30 queries with
+# 100 results each, or 300 queries with 10 results each). It has a limit of 150
+# queries per 15 minutes. Each call to search() counts as one query.
+
+i = None
+for x in range(count / 10):
+	for tweet in twitter.search(keyword, start=x, count=10):
+		i = tweet.id
+		t = i + ' | ' + tweet.text.replace('\n','') + ' | \n'
+		stdout.write('.')
+		if (x % 5) == 0:
+			with open('corpus.txt', 'a') as f:
+				f.write(t.encode('utf-8'))
+				stdout.write('*')
